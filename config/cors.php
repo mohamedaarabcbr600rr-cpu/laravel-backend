0f8c1b5f7d216617959b034ccfacc83716b3ac29
Simo <mohamedaarab.cbr600rr@gmail.com>
2026-05-25 05:28:25 +0100

    fix CORS for production

--- config/cors.php
+++ config/cors.php
@@ -10,7 +10,8 @@ return [
         'http://localhost:5133',   // React (Vite)
         'http://127.0.0.1:5133',
         'http://localhost:8000',   // Laravel نفسه
-    ],
+        'https://react-frontend-production-b8cd.up.railway.app', // React (Railway)
+        ],
 
     'allowed_origins_patterns' => [],
 
